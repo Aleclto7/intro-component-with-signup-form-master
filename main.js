c42a56e4aa3530ac840a1ef0914598ea915f0bf6
Alexis Calixto <alexiscfx1@gmail.com>
2023-11-27 11:47:19 -0300

    icon and error message

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
+const imputs = document.querySelectorAll('.field__input')
 // hide labels message
-const imputs = document.querySelectorAll('.field__input').forEach(element => {
+imputs.forEach(element => {
     element.addEventListener('input', () => {
         if (element.value <=0 ) {
             element.previousElementSibling.classList.remove('inactive');
     }
-    else {
+        else {
         element.previousElementSibling.classList.add('inactive');
     }
 })
 });
+// icon and error message 
+const btn = document.querySelector('.form__button').addEventListener('click', () => {
+    imputs.forEach(element => {
+        if (element.checkValidity()) {
+            element.previousElementSibling.firstElementChild.classList.add('inactive');
+            element.classList.remove('field__error');
+            element.nextElementSibling.classList.add('inactive');
+        }
+        else {
+            element.previousElementSibling.firstElementChild.classList.remove('inactive');
+            element.classList.add('field__error');
+            element.nextElementSibling.classList.remove('inactive');
+        }
+    })
+})
